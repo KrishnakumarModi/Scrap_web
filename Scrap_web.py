b713d50ca98a81be36adb31d450f111a41b135f1
Raja RAM Modi <modikrishna852@gmail.com>
2024-10-12 14:56:04 +0530

    completed

--- Scrap_web.py
+++ Scrap_web.py
@@ -51,32 +51,35 @@ def next_Page():
 
 }"""
 
-for i in range(5):
-    print("scraping page",i+1)
-    product_Box = browser.find_elements(By.XPATH, '//div[@class="sc-19767e73-1 fCFkgQ grid"]')
-    next_Page()
+
 
 data =[]
 
 
-for product in product_Box:     
-    #SKU = product.find_element(By.XPATH, '//div[@class="modelNumber"]').text.strip() #
-   # name = product.find_element(By.XPATH, '//div[@class="sc-66eca60f-24 fPskJH"]').text.strip() #
-    #brand = product.find_element(By.XPATH, '//div[@data-qa="product-name"]').text.strip() #
-    average = product.find_element(By.XPATH, '//div[@class="sc-2709a77c-2 hUinXQ"]').text.strip() #
-    rating = product.find_element(By.XPATH, '//span[@class="sc-2709a77c-5 kwLXrK"]').text.strip() #
-    sponser = product.find_element(By.XPATH, '//div[@class ="sc-66eca60f-23 AkmCS"]').text.strip() #
-    price = product.find_element(By.XPATH, '//span[@class ="oldPrice"]').text.strip() #
-    sales_price = product.find_element(By.XPATH, '//div[@class ="sc-8df39a2e-1 hCDaLm"]').text.strip() #
-    express = product.find_element(By.XPATH, '//div[@class ="sc-cf5a3a41-0 eVCkvW"]').text.strip() #
-    rank = product.find_element(By.XPATH, '//div[@class="sc-66eca60f-24 fPskJH"]').text.strip() 
-    links = product.find_element(By.XPATH, '//div[@class="sc-66eca60f-24 fPskJH"]').text.strip() 
+
+for i in range(2):
+    next_Page()
+    print("scraping page",i+1)
+    product_Link = browser.find_elements(By.XPATH, '//div[@class ="sc-66eca60f-0 EYQUt"]')   #//div[@class="sc-19767e73-1 fCFkgQ grid"]    
+    for product in product_Link:
+        #SKU = product.find_element(By.CLASS_NAME, 'modelNumber').text.strip() #
+        name = product.find_element(By.XPATH, '//div[@data-qa="product-name"]').text.strip() #//div[@class ="sc-66eca60f-24 fPskJH"]
+        brand = product.find_element(By.XPATH, '//div[@class="sc-b74d844-16 cNYrWp"]').text.strip() #
+        average = product.find_element(By.XPATH, '//div[@class="sc-2709a77c-2 hUinXQ"]').text.strip() #
+        rating = product.find_element(By.XPATH, '//span[@class="sc-2709a77c-5 kwLXrK"]').text.strip() #
+        sponser = product.find_element(By.XPATH, '//div[@class ="sc-66eca60f-23 AkmCS"]').text.strip() #
+        price = product.find_element(By.XPATH, '//span[@class ="oldPrice"]').text.strip() #
+        sales_price = product.find_element(By.XPATH, '//div[@class ="sc-8df39a2e-1 hCDaLm"]').text.strip() #
+        express = product.find_element(By.XPATH, '//div[@class ="sc-cf5a3a41-0 eVCkvW"]').text.strip() #
+        rank = product.find_element(By.XPATH, '//div[@class="sc-66eca60f-24 fPskJH"]').text.strip() 
+        links = product.find_element(By.ID, 'productBox-ZE302022BCDD355B5D1DFZ').text.strip() #//div[@class ="sc-19767e73-0 bwele"]
         
 
     data.append({"""'SKU':SKU,"""'Name': name, 'Brand':brand, 'Average Rating':average,'Rating Count': rating, 'Sponsered': sponser, 'Price':price,'Sales Price':sales_price, 'Express':express,'Rank':rank, 'link':links})
 
 print(data)
-# df=pd.DataFrame(data)
+df=pd.DataFrame(data)
+df_to_csv(products.csv)
 
 
 
